8bb89012f9ccf432441734995c7bae6248d2b445
2k19god <james.miggins@hgs.hiddengeniusproject.org>
2018-09-17 19:27:48 -0700

    sucessfull

---
+++ bash.sh
@@ -0,0 +1,24 @@
+pwd					# shows the pathways 
+cd ~/desktop		#takes you you your deskyop
+mkdir website_filesystem			#makes a folder
+cd website_filesystem				#cd change folders to another
+touch index.html					#touch means to make a file making an file called index.html
+mkdir html css js img extra 		#make folders
+cd html								# changing to go to folder
+touch pg1.html						#make a file in html
+touch pg2.html						#make a file in html
+cd ../css							# changing to go to folder
+touch pg1.css						#make a file in css
+touch pg2.css						#make a file in css
+touch index.css						#make a file in css
+cd ../js							#changing to js folder
+touch pg1.js
+touch pg2.js						# making a js file
+touch index.js						# making a js file
+cd ../extra 						#changing to go to extra folder
+mkdir audio							#making a folder 
+mkdir video						#making a folder
+cd ../img/
+curl -o img_1.jpg "https://static.gamespot.com/uploads/screen_small/1587/15875895/3423549-trailer_2k19_gameplay_20180807.jpg"
+curl -o img_2.jpg "https://static-cdn.jtvnw.net/ttv-boxart/Fortnite.jpg"
+curl -o img_3.jpg "http://www.hiddengeniusproject.org/wp/wp-content/uploads/2018/05/HiddenGenius-Spotlight-Zebreon-new.png"
